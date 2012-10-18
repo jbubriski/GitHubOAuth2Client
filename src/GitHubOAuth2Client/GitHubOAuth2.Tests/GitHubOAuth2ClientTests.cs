6ade58253dff27d3a8e0a01eab32fe3e3c43be09
John Bubriski <bubriski@gmail.com>
2012-10-18 14:40:27 -0400

    Moves all the things!!! Adds unit test project

---
+++ src/GitHubOAuth2Client/GitHubOAuth2.Tests/GitHubOAuth2ClientTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using JohnnyCode;
+
+namespace GitHubOAuth2.Tests
+{
+    public class GitHubOAuth2ClientTests
+    {
+        public void Ctor_WithNullAppId_ThrowsException()
+        {
+            // Arrange
+
+            // Act
+            var gitHubOAuth2Client = new GitHubOAuth2Client(null, "something");
+
+            // Assert
+
+        }
+    }
+}
