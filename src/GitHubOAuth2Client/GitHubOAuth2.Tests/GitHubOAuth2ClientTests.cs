fa9a120a3b9a7fb360f274687120f42629d2e41b
John Bubriski <bubriski@gmail.com>
2012-10-18 14:51:42 -0400

    Adds XUnit and initial tests

--- src/GitHubOAuth2Client/GitHubOAuth2.Tests/GitHubOAuth2ClientTests.cs
+++ src/GitHubOAuth2Client/GitHubOAuth2.Tests/GitHubOAuth2ClientTests.cs
@@ -4,20 +4,43 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using JohnnyCode;
+using Xunit;
 
 namespace GitHubOAuth2.Tests
 {
     public class GitHubOAuth2ClientTests
     {
+        [Fact]
         public void Ctor_WithNullAppId_ThrowsException()
         {
             // Arrange
+            // Act
+            // Assert
+            var argumentNullException = Assert.Throws<ArgumentNullException>(() => { new GitHubOAuth2Client(null, "something"); });
+
+            Assert.Equal("appId", argumentNullException.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_WithNullAppSecret_ThrowsException()
+        {
+            // Arrange
+            // Act
+            // Assert
+            var argumentNullException = Assert.Throws<ArgumentNullException>(() => { new GitHubOAuth2Client("something", null); });
+
+            Assert.Equal("appSecret", argumentNullException.ParamName);
+        }
+
+        [Fact]
+        public void Ctor_SetsCorrectProviderName()
+        {
+            // Arrange
+            var gitHubOAuth2Client = new GitHubOAuth2Client("something", "something");
 
             // Act
-            var gitHubOAuth2Client = new GitHubOAuth2Client(null, "something");
-
             // Assert
-
+            Assert.Equal("github", gitHubOAuth2Client.ProviderName);
         }
     }
 }
