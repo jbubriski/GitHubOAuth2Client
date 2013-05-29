41d03062e48261b98e7a3c8373ea558cab22f226
Ruslan Konviser <evereq@gmail.com>
2013-05-29 13:42:08 +0300

    Add required "user-agent" value for every request to Github API using ExtendedWebClient. Upgrade packages to recent versions.

---
+++ src/GitHubOAuth2Client/GitHubOAuth2/ExtendedWebClient.cs
@@ -0,0 +1,21 @@
+﻿using System.Net;
+
+namespace JohnnyCode.GitHubOAuth2
+{
+    /// <summary>
+    /// Extended WebClient. Set Encoding to UTF-8, add user-agent request header and set null proxy for faster networking
+    /// </summary>
+    public class ExtendedWebClient : WebClient
+    {
+        /// <summary>
+        /// Initialize WebClient and adds user-agent requests header
+        /// </summary>
+        /// <param name="userAgent"></param>
+        public ExtendedWebClient(string userAgent)
+        {
+            Proxy = null;
+            Encoding = System.Text.Encoding.UTF8;
+            Headers.Add("user-agent", userAgent);
+        }       
+    }
+}
